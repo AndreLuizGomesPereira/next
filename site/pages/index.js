1f1e60781e874c70f818a4df85723d213a09d01d
André Luiz <andregomesenator@gmail.com>
2020-07-01 18:09:16 -0300

    Aplicado estilo Navbar.

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
-
-
+import {
+    Jumbotron,
+    Button,
+    Collapse,
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function HomePage() {
+
     return (
         <div>
+            <Navbar color="secondary" dark expand="md">
+                <NavbarBrand href="/">André Luiz</NavbarBrand>
+                <Collapse navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/components/">Componentes</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/AndreLuizGomesPereira">GitHub - André Luiz</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
             <Jumbotron>
                 <h1 className="display-3">Hello, world!</h1>
                 <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
