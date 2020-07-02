4e0abbb26780016ea1a42967b056f913f4cdece9
André Luiz <andregomesenator@gmail.com>
2020-07-02 20:09:31 -0300

    Inclusão FontAwesome.

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ import {
     NavLink,
 } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas)
 
 
 function HomePage() {
